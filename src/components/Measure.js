4dd5858dce423d8c1127af430c0d5c3a01b8caca
André Lima <hello@andrelima.space>
2019-08-02 14:47:58 -0300

    Finished functionality

--- src/components/Measure.js
+++ src/components/Measure.js
@@ -2,25 +2,43 @@ import React, { useState, useEffect } from 'react';
 
 const Measure = (props) => {
     const { callback } = props;
-    const [ clicks, setClicks ] = useState([]);
+    const [clicks, setClicks] = useState([]);
+    const [bpm, setBpm] = useState(60);
+    let zeroTimeout;
 
     const handleKey = event => {
-        const time = new Date().valueOf();
-        const newClicks = clicks;
-        newClicks.push({ time });
+        if (event.key === 'Control') {
+            const time = new Date().valueOf();
+            const newClicks = clicks;
+            newClicks.push({ time });
 
-        setClicks([...newClicks]);
-        calculateBpm();
+            setClicks([...newClicks]);
+            calculateBpm();
+        }
+    };
+
+    const handleType = event => {
+        const newBpm = parseInt(event.target.value);
+
+        setBpm(newBpm);
+        callback(newBpm);
     };
 
     const calculateBpm = () => {
+        clearTimeout(zeroTimeout);
+
         const length = clicks.length;
         const difference = clicks[length - 1].time - clicks[0].time;
         const average = difference / length;
         const avgSeconds = 1000 / average;
-        const bpm = 60 * avgSeconds;
-        
-        callback(bpm);
+        const newBpm = parseInt(60 * avgSeconds);
+
+        setBpm(newBpm);
+        callback(newBpm);
+
+        zeroTimeout = setTimeout(() => {
+            setClicks([]);
+        }, 5000);
     };
 
     useEffect(() => {
@@ -33,7 +51,8 @@ const Measure = (props) => {
 
     return (
         <div>
-            <h2>Measure</h2>
+            <h2>Type the BPM or tap CTRL to count it</h2>
+            <input type="number" value={bpm} onChange={handleType} />
         </div>
     );
 };
--- src/components/Player.js
+++ src/components/Player.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+const audioUrl = require('../assets/click.wav');
 
 const Player = (props) => {
     const { bpm } = props;
+    const [playing, setPlaying] = useState(false);
+    const audio = new Audio(audioUrl);
+
+    const [playInterval, setPlayInterval] = useState();
+
+    const handlePlay = () => {
+        setPlaying(true);
+
+        const intervalFreq = 60 * 1000 / bpm;
+        
+        setPlayInterval(setInterval(() => {
+            audio.currentTime = 0;
+            audio.play();
+        }, intervalFreq));
+    };
+
+    const handlePause = () => {
+        setPlaying(false);
+        clearInterval(playInterval);
+        setPlayInterval();
+        audio.pause();
+    };
 
     return (
         <div>
             <h2>Player</h2>
-            <p>{bpm}</p>
+            <p>{bpm} bpm</p>
+            {playing ? <button onClick={handlePause}>Pause</button>
+                : <button onClick={handlePlay}>Play</button>
+            }
         </div>
     );
 };
